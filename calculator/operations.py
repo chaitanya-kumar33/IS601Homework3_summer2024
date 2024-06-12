3b192a3594612e3471717aab7c352aa02ac63cd2
chaitanya-kumar33 <chaitanyakumar1922@gmail.com>
2024-06-12 00:56:38 -0500

    implemented operation functions and documentation

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,11 +1,13 @@
+#takes a and b and returns the sum
 def add(a,b):
     return a + b
 
+#addition function
 def subtract(a,b):
     return a - b
-
+#Multiplication function
 def multiply(a,b):
     return a * b
-
+#does the division
 def divide(a,b):
     return a / b
